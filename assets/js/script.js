e51d919087a676347809f315f68176d7596f6a91
mofadl237 <mofadl237@gmail.com>
2025-01-27 20:50:49 +0200

    Update Data Product

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,36 @@
-var productName = document.getElementById("productName");
-var productPrice = document.getElementById("productPrice");
-var productCategory = document.getElementById("productCategory");
-var productDesc = document.getElementById("productDesc");
-var productSearch=document.querySelector("#productSearch");
-
-var contentAdd = document.querySelector("table tbody");
+let productName = document.getElementById("productName");
+let productPrice = document.getElementById("productPrice");
+let productCategory = document.getElementById("productCategory");
+let productDesc = document.getElementById("productDesc");
+let productSearch = document.querySelector("#productSearch");
+let contentAdd = document.querySelector("table tbody");
 //Access 4 button In This Project
-var btn_add = document.getElementById("btn_add");
-var btn_sort = document.getElementById("btn_sort");
+let btn_add = document.getElementById("btn_add");
+let btn_sort = document.getElementById("btn_sort");
+let btn_update = document.querySelector("#btn_update");
 
 /*All Function In One Function Add Data */
 //Add List Product in one Array To Display Array
-var AllProducts = [];
+let AllProducts = [];
+let productUpdate = 0;
 // create object to add data from global to access in any function
 
 /* كدا global يعني التعديل جوا بيعدل عليه ىلازم ارجعه عشن اخد منه نسخه تانيه  حطه جوا داله GetData*/
 
 //0- Get Data From Local Storage
-if(localStorage.getItem('products') != null){
-  AllProducts=JSON.parse(localStorage.getItem('products'));
+if (localStorage.getItem("products") != null) {
+  AllProducts = JSON.parse(localStorage.getItem("products"));
   displayProducts();
 }
 
 //1- get DataFrom User
 function getData() {
-  var product = {
+  return {
     name: productName.value,
     price: productPrice.value,
     category: productCategory.value,
     Desc: productDesc.value,
   };
-  return product;
 }
 //2- check valid data
 function validData() {
@@ -51,16 +51,16 @@ function validData() {
 //4- push object in array list
 function addFirst(productObject) {
   AllProducts.unshift(productObject);
-  localStorage.setItem('products',JSON.stringify(AllProducts));
+  localStorage.setItem("products", JSON.stringify(AllProducts));
 }
 function addLast(productObject) {
   AllProducts.push(productObject);
-  localStorage.setItem('products',JSON.stringify(AllProducts));
+  localStorage.setItem("products", JSON.stringify(AllProducts));
 }
 //5- display data in array for table tbody innerHTML
-function displayArray(Array){
+function displayArray(Array) {
   contentAdd.innerHTML = "";
-  if(Array.length<1){
+  if (Array.length < 1) {
     contentAdd.innerHTML += `<tr > <td colspan="7"  style="text-align: center;">Not Found Product</td> </tr>`;
   }
   for (var i = 0; i < Array.length; i++) {
@@ -70,20 +70,20 @@ function displayArray(Array){
           <td>${Array[i].price}</td>
           <td>${Array[i].category}</td>
           <td>${Array[i].Desc}</td>
-          <td><button class="btn btn-warning" data-item="${i}" id="btn_update" onclick="updateProduct(${i})">Update</button></td>
+          <td><button class="btn btn-outline-warning text-white" data-item="${i}" id="btn_update" onclick="loadDataChange(${i})">Update</button></td>
           <td><button class="btn btn-danger"  data-item="${i}" id="btn_delete" onclick="deleteProduct(${i});">Delete</button></td>
       </tr>`;
-}
+  }
 }
 function displayProducts() {
-    //Clear  Content Data
-    
-    if(AllProducts.length > 1){
-      btn_sort.style.visibility="visible";
-    }else{
-      btn_sort.style.visibility="hidden";
-    }
-    displayArray(AllProducts);
+  //Clear  Content Data
+
+  if (AllProducts.length > 1) {
+    btn_sort.style.visibility = "visible";
+  } else {
+    btn_sort.style.visibility = "hidden";
+  }
+  displayArray(AllProducts);
 }
 //6- clear data from inputs
 function clearData() {
@@ -110,21 +110,30 @@ function sortedArray() {
 
 //8- Delete Function
 function deleteProduct(i) {
-  AllProducts.splice(i,1);
-  localStorage.setItem('products',JSON.stringify(AllProducts));
+  AllProducts.splice(i, 1);
+  localStorage.setItem("products", JSON.stringify(AllProducts));
   displayProducts();
 }
 
 //9- Update Function
-function updateProduct(i){
-    validData();
-    var product=getData();
-    AllProducts.splice(i,1,product);
-    localStorage.setItem('products',JSON.stringify(AllProducts));
-    displayProducts();
-    clearData();
-}
+function loadDataChange(i) {
+  btn_add.classList.add("d-none");
+  btn_update.classList.remove("d-none");
 
+  productCategory.value = AllProducts[i].category;
+  productDesc.value = AllProducts[i].Desc;
+  productName.value = AllProducts[i].name;
+  productPrice.value = AllProducts[i].price;
+  productUpdate = i;
+}
+function updateData() {
+  btn_add.classList.remove("d-none");
+  btn_update.classList.add("d-none");
+  AllProducts[productUpdate] = getData();
+  localStorage.setItem("products", JSON.stringify(AllProducts));
+  clearData();
+  displayProducts();
+}
 /*This Is Most Function In This Project */
 
 //3-function AddProduct
@@ -136,18 +145,18 @@ function AddProduct() {
   displayProducts();
 }
 //10- Search Data
-function searchData(){
+function searchData() {
   var search = productSearch.value;
-  var ProductSearchArray=[];
-  for(var i=0;i<AllProducts.length;i++){
-    if(AllProducts[i].name.toLowerCase().includes(search.toLowerCase())){
+  var ProductSearchArray = [];
+  for (var i = 0; i < AllProducts.length; i++) {
+    if (AllProducts[i].name.toLowerCase().includes(search.toLowerCase())) {
       ProductSearchArray.push(AllProducts[i]);
     }
   }
   displayArray(ProductSearchArray);
 }
-productSearch.addEventListener("input",searchData);
+productSearch.addEventListener("input", searchData);
 //Add Event On Buttons
 btn_add.addEventListener("click", AddProduct);
 btn_sort.addEventListener("click", sortedArray);
-
+btn_update.addEventListener("click", updateData);
